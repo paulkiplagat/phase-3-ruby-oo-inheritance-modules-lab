abb787fb5c16d4602727e7979f897488bf10b203
Paul <paul.kiplagt@student.moringaschool.com>
2022-08-19 14:08:35 +0300

    done

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,6 +3,12 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
+  
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
 
   @@artists = []
 
